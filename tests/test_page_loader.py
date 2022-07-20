6881255bbdcc6e632ffc0ea9e458eb41f856dcda
shitcoding <shitcoder@pm.me>
2022-07-20 12:55:00 +0300

    change fixture in page download test

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,32 +1,38 @@
-import os
-from page_loader import download
+from pathlib import Path
 import pytest
 import requests_mock
 import tempfile
 
+from page_loader import download
+
 
 def get_path(fixture_filename):
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(current_dir, 'fixtures', fixture_filename)
+    current_dir = Path(__file__).parent.absolute()
+    return current_dir / 'fixtures' / fixture_filename
 
 
 def read_fixture(filename):
-    path = get_path(filename)
-    with open(path, 'r') as f:
+    fixture_path = get_path(filename)
+    with open(fixture_path, 'r') as f:
         data = f.read()
         return data.rstrip()
 
+
 @pytest.fixture
-def hexlet_courses_page():
-    return read_fixture('ru-hexlet-io-courses.html').strip()
+def realpython_page():
+    return read_fixture('realpython-com.html').strip()
 
 
-def test_download_page(hexlet_courses_page, requests_mock):
-    requests_mock.get('https://ru.hexlet.io/courses', text=hexlet_courses_page)
+def test_download_page(realpython_page, requests_mock):
+    # mocking the request to target page
+    requests_mock.get('https://realpython.com', text=realpython_page)
+    # saving target page html to temporary directory
     with tempfile.TemporaryDirectory() as tmpdir:
-        output_path = download('https://ru.hexlet.io/courses', tmpdir)
+        output_path = download('https://realpython.com', tmpdir)
         with open(output_path) as f:
             output = f.read().strip()
-            assert output == hexlet_courses_page
+            # checking if saved html is the same as the html fixture
+            assert output == realpython_page
+        # checking that mock request has been called once
         assert requests_mock.called
         assert requests_mock.call_count == 1
